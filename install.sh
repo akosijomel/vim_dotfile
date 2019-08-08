5e3abad95dca42d671a003bc75f660180661dd75
jomel.madia <jomel.madia@gmail.com>
2019-08-09 00:33:44 +0800

    change respond

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ if [[ -e /home/$USER/.vimrc ]]; then
   rm /home/$USER/.vimrc
   download_from_github
   echo  "Your .vimrc file was replaced"
-  echo  "Run `:PlugInstall` inside vim"
+  echo  "Run \`:PlugInstall\` inside vim"
 else
   download_from_github
-  echo ".vimrc file was downloaded from the repository"
-  echo  "Run `:PlugInstall` inside vim"
+  echo ".vimrc file was downladed from the repository"
+  echo  "Run \`:PlugInstall\` inside vim"
 fi
